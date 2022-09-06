5c01db1cdf6e5d45afda9b21f9e86626e1dccf12
mnyazar <yazarmusa@gmail.com>
2022-09-06 10:30:02 +0300

    Longtiduinal Driver is added

--- setVel.m
+++ setVel.m
@@ -2,8 +2,8 @@ function setVel(in)
 %SETLOOKAD Summary of this function goes here
 %   Detailed explanation goes here
 
-set_param('exercise_modelv2/Pure Pursuit Controller/Pure Pursuit','DesiredLinearVelocity',num2str(in));
-set_param('exercise_modelv2/velocity','Value',num2str(in));
+set_param('MultiAgentModel/Pure Pursuit Controller/Pure Pursuit','DesiredLinearVelocity',num2str(in));
+set_param('MultiAgentModel/velocity','Value',num2str(in));
 
 end
 
